939ac2a09c64d7f9e82b0844dfb7f3950ce77eb6
Spencer Mathews <smathews0906@gmail.com>
2022-08-15 04:14:05 -0500

    commit tree a75384c1bd727114d454b9373db8a07b80083b11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
     render json: user, include: :items
   end
 
+  def items_index
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items, include: :user
+  end
+
 end
